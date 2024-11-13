00aa79182797e3ee5d190a1b2e43ce3dafc952a8
DAFAMD2007 <tharindudilruksha115@gmail.com>
2024-11-13 11:35:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ module.exports = {
   // public = AnyOne Working
   // inbox = Only Working  Inbox
   // groups = only working in group
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "94724087485",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "94759900301",
   //========================================- OTHER - CONFIGS -=====================================================================
   AUTO_VOICE: process.env.AUTO_VOICE || "true",
   ANTI_BAD_WORDS_ENABLED: process.env.ANTI_BAD_WORDS_ENABLED || "true",
@@ -29,7 +29,7 @@ module.exports = {
   ALWAYS_RECORDING: process.env.ALWAYS_RECORDING || "true",
   ANTI_BOT: process.env.ANTI_BOT || "true",
   ANTI_DELETE: process.env.ANTI_DELETE || "true",
-  packname: process.env.packname || "DAFA MD",
+  packname: process.env.packname || "QUEEN YESHU",
   author: process.env.author || "QUEEN YESHU",
   //==========================================- API-CONFIGS -==========================================================
   OPENWEATHER_API_KEY: process.env.OPENWEATHER_API_KEY || "2d61a72574c11c4f36173b627f8cb177", //openweathermap.org
